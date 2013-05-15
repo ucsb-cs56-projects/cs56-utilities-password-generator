cc7128ed399a1dff85b0e9c14f2f7defcfebe55f
Ian Vernon <ianvernon@umail.ucsb.edu>
2013-05-15 10:27:53 -0700

    added Javadoc for GUI class

--- src/PasswordGUI.java
+++ src/PasswordGUI.java
@@ -2,6 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+/**
+   PasswordGUI represents a GUI for interacting with the Password class. A Password contains digits, upper case letters, and special characters. One can set the length of the Password to be greater than or equal to three. A Password is generated with the Generate button. 
+*/
+
 public class PasswordGUI
 {
     private JButton button;
@@ -11,13 +15,17 @@ public class PasswordGUI
     private JFrame frame;
     private JTextField passwordOutputField;
 
+    /**
+       launches the JFrame, populates it with the password length field, generate button, and generated password text field
+    */
+
     public void go(){
         frame = new JFrame();
 	frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	p = new Password();
 
-	greeting = new JTextField("Please enter the length of the password (>3 characters):", 30);
+	greeting = new JTextField("Please enter the length of the password (>= 3 characters):", 30);
         lengthField = new JTextField("");
         button = new JButton("Generate");
 	passwordOutputField = new JTextField("");
@@ -33,6 +41,10 @@ public class PasswordGUI
         frame.setSize(400,300);
         frame.setVisible(true);
     }
+    
+    /** 
+	inner class for Generate button
+    */
 
     public class ButtonListener implements ActionListener
     {
@@ -41,6 +53,10 @@ public class PasswordGUI
 	    onActionPerformed();
 	}
     }
+    /**
+       inner class for password length field
+    */
+
     public class TextFieldListener implements ActionListener
     {
         public void actionPerformed(ActionEvent event)
@@ -48,14 +64,17 @@ public class PasswordGUI
 	    onActionPerformed();
 	}
     }
+    
+    /**
+       method that checks length field for valid input. if length field has valid input, generates password with that length, and sets text of generated password field with that password
+    */
+
     public void onActionPerformed() 
     {
 	int length = 0;
 	try
 	    {
 		length = Integer.parseInt(lengthField.getText());
-		//System.out.println(length);
-		//	p.setPassLength(length);
 	    }
 	catch(NumberFormatException e)
 	    {
